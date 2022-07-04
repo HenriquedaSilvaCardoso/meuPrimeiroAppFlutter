f18203509d4ab83704cb229abc389cf331c72fcb
Henrique da Silva Cardoso <henriquedasilvacardoso00@gmail.com>
2022-07-04 14:01:51 -0300

    [feat] Created my first Flutter app

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    home: HomePage(),
+  ));
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: const Center(
+        child: Text('Olá mundo!'),
+        ),
+    );
+  }
+}
